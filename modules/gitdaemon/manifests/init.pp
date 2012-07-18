da7a2b03c2e1d0cbff9e9f2598343b70b2c01472
Rob Spanton <rspanton@zepler.net>
2012-07-18 02:42:53 +0100

    Enable the git daemon in the xinetd config
    Just install a new xinetd git file.

--- modules/gitdaemon/manifests/init.pp
+++ modules/gitdaemon/manifests/init.pp
@@ -11,11 +11,26 @@ class gitdaemon {
     ensure => running,
   }
 
-  package { "git-daemon":
-      ensure => latest,
-  }
-
+  # Restart xinetd after package updates
   Package["xinetd"] ~> Service["xinetd"]
 
+  package { "git-daemon":
+    ensure => latest,
+    require => Package["xinetd"],
+  }
+
+  # The xinetd git daemon config file
+  file { "/etc/xinetd.d/git":
+    mode => 644,
+    owner => root,
+    group => root,
+    source => "puppet:///modules/gitdaemon/git.xinetd",
+
+    # Install after git-daemon package
+    require => Package["git-daemon"],
+
+    # Restart xinetd when this file changes
+    notify => Service["xinetd"],
+  }
 
 }
