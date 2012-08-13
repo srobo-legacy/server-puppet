8c9d5254e318e21e47b9b100012afd2ec34f6156
Rob Spanton <rspanton@zepler.net>
2012-08-13 01:52:32 +0100

    Add beginnings of httpd.conf
    Almost identical right now to the one that ships with the httpd
    package.

--- modules/www/manifests/httpd.pp
+++ modules/www/manifests/httpd.pp
@@ -5,12 +5,20 @@ class www::httpd {
     ensure => latest,
   }
 
+  file { "httpd.conf":
+    path => "/etc/httpd/conf/httpd.conf",
+    owner => root,
+    group => root,
+    mode => "0600",
+    source => "puppet:///modules/www/httpd.conf",
+  }
+
   service { "httpd":
     enable => true,
     ensure => running,
-    subscribe => Package[ "httpd" ],
+    subscribe => [ Package[ "httpd" ],
+                   File[ "httpd.conf" ] ],
   }
 
-  
 
 }
