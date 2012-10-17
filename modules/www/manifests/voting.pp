eed37987b706bfa3990679ae0da55e6cac52d18a
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-17 02:26:41 +0100

    Voting script requires yaml.

--- modules/www/manifests/voting.pp
+++ modules/www/manifests/voting.pp
@@ -1,9 +1,14 @@
 class www::voting ($git_root, $web_root_dir) {
+  package { 'PyYAML':
+    ensure => present,
+  }
+
   vcsrepo { "${web_root_dir}/voting":
     ensure => present,
     provider => git,
     source => "${git_root}/voting.git",
     revision => "master",
     force => true,
+    require => Package['PyYAML'],
   }
 }
