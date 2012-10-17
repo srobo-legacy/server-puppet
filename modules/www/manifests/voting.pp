6a6d2d790a8e024e4c1dc358a567847a3b4dc8cd
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-17 16:14:07 +0100

    Actually, install voting stuff in a users homedir
    This is because the apache goo require to make suexec work turns out to
    be difficult to configure on a per-file or per-dir basis. It can only
    (apparently) work on vhosts or on user dirs. So to keep the owner of the
    voting files seperate from the rest of the website, it _has_ to go in a
    home directory.

--- modules/www/manifests/voting.pp
+++ modules/www/manifests/voting.pp
@@ -3,12 +3,30 @@ class www::voting ($git_root, $web_root_dir) {
     ensure => present,
   }
 
-  vcsrepo { "${web_root_dir}/voting":
+  file { '/home/voting':
+    ensure => directory,
+    owner => 'voting',
+    group => 'users',
+    mode => '711',
+    require => User['voting'],
+  }
+
+  user { 'voting':
+    ensure => present,
+    comment => 'Owner of voting record files',
+    shell => '/sbin/nologin',
+    gid => 'users',
+    home => '/home/voting',
+  }
+
+  vcsrepo { "/home/voting/public_html/voting":
     ensure => present,
     provider => git,
     source => "${git_root}/voting.git",
     revision => "master",
     force => true,
-    require => Package['PyYAML'],
+    require => [Package['PyYAML'], User['voting']],
+    owner => 'voting',
+    group => 'users',
   }
 }
