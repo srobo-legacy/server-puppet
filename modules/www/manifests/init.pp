c89cdaf1508214654b2f290a814344063a68e95c
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-18 21:53:24 +0100

    Install wwwcontent user before content

--- modules/www/manifests/init.pp
+++ modules/www/manifests/init.pp
@@ -22,15 +22,18 @@ class www( $git_root ) {
   class { "www::srweb":
     git_root => $git_root,
     web_root_dir => $web_root_dir,
+    require => User['wwwcontent'],
   }
 
   class { "www::voting":
     git_root => $git_root,
     web_root_dir => $web_root_dir,
+    require => User['wwwcontent'],
   }
 
   class { 'www::phpbb':
     git_root => $git_root,
     root_dir => '/var/www/vhosts/phpbb',
+    require => User['wwwcontent'],
   }
 }
