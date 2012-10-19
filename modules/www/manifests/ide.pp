a2fecadabc2440f15176660a821c339e712a59c7
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-19 01:31:31 +0100

    Install a secret-key for IDE cookies.

--- modules/www/manifests/ide.pp
+++ modules/www/manifests/ide.pp
@@ -4,6 +4,8 @@ class www::ide ( $git_root, $root_dir ) {
     before => Vcsrepo["${root_dir}"],
   }
 
+  # NB: the applet is deliberately unconfigured because everyone I speak to
+  # doesn't want it to exist any more.
   vcsrepo { "${root_dir}":
     ensure => present,
     provider => git,
@@ -13,5 +15,13 @@ class www::ide ( $git_root, $root_dir ) {
     owner => 'wwwcontent',
     group => 'apache',
     require => Class['srweb'],
+  }
+
+  file { "${root_dir}/config/ide-key.key":
+    ensure => present,
+    owner => 'wwwcontent',
+    group => 'apache',
+    mode => '640',
+    content => extlookup('ide_cookie_key'),
   }
 }
