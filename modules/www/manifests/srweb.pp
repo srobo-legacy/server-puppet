dbe630f76ce828ccbc33832af2292efd2411e4d8
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-24 19:41:41 +0100

    Ensure srwebs directory has the right permissions.
    Seeing how we're going to clone into it, and the parent directory is
    owned by root, pre-make it with the correct owner/mode.

--- modules/www/manifests/srweb.pp
+++ modules/www/manifests/srweb.pp
@@ -12,6 +12,15 @@ class www::srweb ( $git_root, $web_root_dir ) {
     hasstatus => 'true',
   }
 
+  # Ensure website dir exists and is owned by wwwcontent
+  file { "${web_root_dir}":
+    ensure => directory,
+    owner => 'wwwcontent',
+    group => 'apache',
+    mode => '755',
+    before => Vcsrepo["${web_root_dir}"],
+  }
+
   # Maintain a git clone of the website
   vcsrepo { "${web_root_dir}":
     ensure => present,
