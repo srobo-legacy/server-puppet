47a9a7f4dd10f65ed825ff24dd3309da16129618
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-07 09:40:51 +0000

    Create gerrit user and download the latest ver.
    We're going to have to do updates manually. Too bad.

--- modules/sr-site/manifests/gerrit.pp
+++ modules/sr-site/manifests/gerrit.pp
@@ -1,2 +1,15 @@
 class sr-site::gerrit {
+  user { 'gerrit':
+    ensure => present,
+    comment => 'Owner of all gerrit specific files/data',
+    shell => '/bin/sh', # Has to log in successfully, as it runs java.
+    gid => 'users',
+  }
+
+  exec { 'download-gerrit':
+    command => 'curl http://gerrit.googlecode.com/files/gerrit-full-2.5.war > /home/gerrit/gerrit-full-2.5.war',
+    creates => '/home/gerrit/gerrit-full-2.5.war',
+    user => 'gerrit',
+    require => User['gerrit'],
+  }
 }
