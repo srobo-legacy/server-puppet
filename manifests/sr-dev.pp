d16dcf0c51d62a8282ae210c04b221ead4a267d3
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-09 23:27:24 +0000

    Allow manifests to change behaviour between dev/server mode.

--- manifests/sr-dev.pp
+++ manifests/sr-dev.pp
@@ -1,5 +1,6 @@
 $extlookup_datadir = "/srv/secrets"
 $extlookup_precedence = [ "common" ]
+$devmode = 1
 
 class { "sr-site":
   git_root => "git://srobo.org",
--- manifests/sr-server.pp
+++ manifests/sr-server.pp
@@ -1,5 +1,6 @@
 $extlookup_datadir = "/srv/secrets"
 $extlookup_precedence = [ "common" ]
+$devmode = 0
 
 class { "sr-site":
   git_root => "/srv/git",
