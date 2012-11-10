d8d09d1e8550d54f9d9c8593bc222938af886494
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-10 18:29:20 +0000

    Disable vast amounts of git RSS goo.
    Previously this generated RSS data for people pushing to git, and
    for commits that were pushed into git. However, in our glorious
    gerrit future it's unclear how this is going to work, so disable all
    of that for now.

--- modules/sr-site/manifests/git.pp
+++ modules/sr-site/manifests/git.pp
@@ -60,74 +60,74 @@ class sr-site::git($git_root) {
     ensure => present,
   }
 
-  package { 'python-pyrss2gen.noarch':
-    ensure => present,
-    provider => rpm,
-    source => '/root/python-pyrss2gen.noarch.rpm',
-   }
+#  package { 'python-pyrss2gen.noarch':
+#    ensure => present,
+#    provider => rpm,
+#    source => '/root/python-pyrss2gen.noarch.rpm',
+#   }
 
-  file { '/root/python-pyrss2gen.noarch.rpm':
-    ensure => present,
-    owner => root,
-    mode => 400,
-    source => 'puppet:///modules/sr-site/python-pyrss2gen-1.0.0-2.2.noarch.rpm',
-    before => Package['python-pyrss2gen.noarch'],
-  }
+#  file { '/root/python-pyrss2gen.noarch.rpm':
+#    ensure => present,
+#    owner => root,
+#    mode => 400,
+#    source => 'puppet:///modules/sr-site/python-pyrss2gen-1.0.0-2.2.noarch.rpm',
+#    before => Package['python-pyrss2gen.noarch'],
+#  }
 
-  cron { 'commitrss':
-    command => '/srv/git/scripts/rss',
-    minute => '*/15',
-    user => 'git',
-    require => [Vcsrepo['/srv/git/scripts'], Package['GitPython','python-pyrss2gen.noarch']],
-  }
+#  cron { 'commitrss':
+#    command => '/srv/git/scripts/rss',
+#    minute => '*/15',
+#    user => 'git',
+#    require => [Vcsrepo['/srv/git/scripts'], Package['GitPython','python-pyrss2gen.noarch']],
+#  }
 
-  cron { 'pushrss':
-    command => '/srv/git/scripts/pushlog-rss',
-    minute => '10',
-    user => 'git',
-    require => [Vcsrepo['/srv/git/scripts'], Package['GitPython','python-pyrss2gen.noarch']],
-  }
+#  cron { 'pushrss':
+#    command => '/srv/git/scripts/pushlog-rss',
+#    minute => '10',
+#    user => 'git',
+#    require => [Vcsrepo['/srv/git/scripts'], Package['GitPython','python-pyrss2gen.noarch']],
+#  }
 
-  file { '/srv/git/commits.rss':
-    ensure => present,
-    owner => 'git',
-    group => 'srusers',
-    mode => '644',
-    before => Vcsrepo['/srv/git/scripts'],
-  }
+#  file { '/srv/git/commits.rss':
+#    ensure => present,
+#    owner => 'git',
+#    group => 'srusers',
+#    mode => '644',
+#    before => Vcsrepo['/srv/git/scripts'],
+#  }
 
-  file { '/srv/git/pushes.rss':
-    ensure => present,
-    owner => 'git',
-    group => 'srusers',
-    mode => '644',
-    before => Vcsrepo['/srv/git/scripts'],
-  }
+#  file { '/srv/git/pushes.rss':
+#    ensure => present,
+#    owner => 'git',
+#    group => 'srusers',
+#    mode => '644',
+#    before => Vcsrepo['/srv/git/scripts'],
+#  }
 
-  file { '/srv/git/push-log':
-    ensure => present,
-    owner => 'root',
-    group => 'git-commit',
-    mode => '664',
-    before => Vcsrepo['/srv/git/scripts'],
-  }
+#  file { '/srv/git/push-log':
+#    ensure => present,
+#    owner => 'root',
+#    group => 'git-commit',
+#    mode => '664',
+#    before => Vcsrepo['/srv/git/scripts'],
+#  }
 
-  file { '/srv/git/update-log':
-    ensure => present,
-    owner => 'root',
-    group => 'git-commit',
-    mode => '664',
-    before => Vcsrepo['/srv/git/scripts'],
-  }
+#  file { '/srv/git/update-log':
+#    ensure => present,
+#    owner => 'root',
+#    group => 'git-commit',
+#    mode => '664',
+#    before => Vcsrepo['/srv/git/scripts'],
+#  }
 
-  file { '/srv/git/repolist':
-    ensure => present,
-    owner => 'root',
-    group => 'git-admin',
-    mode => '664',
-    before => Vcsrepo['/srv/git/scripts'],
-    require => Exec['ldap-groups-flushed'],
-  }
+#  file { '/srv/git/repolist':
+#    ensure => present,
+#    owner => 'root',
+#    group => 'git-admin',
+#    mode => '664',
+#    before => Vcsrepo['/srv/git/scripts'],
+#    require => Exec['ldap-groups-flushed'],
+#  }
 
   package { 'cgit':
     ensure => present,
@@ -140,7 +140,7 @@ class sr-site::git($git_root) {
     require => [Package['cgit'], Exec['ldap-groups-flushed']],
   }
 
-  package { ['perl', 'perl-RPC-XML']:
-    ensure => present,
-  }
+#  package { ['perl', 'perl-RPC-XML']:
+#    ensure => present,
+#  }
 }
