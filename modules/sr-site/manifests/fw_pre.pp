8167847861c85f800fd5646debebaba69f811e7f
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-12-23 16:18:01 +0000

    Document firewall rules.
    They're all pretty self explanatory, but hey, more docs.

--- modules/sr-site/manifests/fw_pre.pp
+++ modules/sr-site/manifests/fw_pre.pp
@@ -12,36 +12,44 @@ class sr-site::fw_pre {
     action => "accept",
   }
 
+  # Allow all traffic attached to established connections. Important for
+  # connections made by the server.
   firewall { "000 INPUT allow related and established":
     state => ["RELATED", "ESTABLISHED"],
     action => "accept",
     proto => "all",
   }
 
+  # Allow everyone to connect to ssh.
   firewall { "002 ssh":
     proto  => "tcp",
     dport => 22,
     action => "accept",
   }
 
+  # Allow everyone to connect to the anonymous git service.
   firewall { "003 git":
     proto => "tcp",
     dport => 9418,
     action => "accept",
   }
 
+  # Allow everyone to connect to the HTTP website.
   firewall { "004 http":
     proto => "tcp",
     dport => 80,
     action => "accept",
   }
 
+  # Allow everyone to connect to the SSL website
   firewall { "005 https":
     proto => "tcp",
     dport => 443,
     action => "accept",
   }
 
+  # Open gerrit's HTTP service to be reverse-proxy'd by apache. It's not
+  # acceptable to have passwords flying over non-SSL connections.
   firewall { "006 gerrit-http":
     proto => "tcp",
     dport => "8081",
@@ -49,6 +57,7 @@ class sr-site::fw_pre {
     source => "127.0.0.1", # Limit to only apache reverse-proxying.
   }
 
+  # Open gerrit's SSHD service to everyone
   firewall { "007 gerrit-sshd":
     proto => "tcp",
     dport => "29418",
