3eb7a489081a537616cf8092c5462194e4ff03cb
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-12-28 23:07:45 +0000

    Scatter comments on how phpbb is configured

--- modules/www/manifests/phpbb.pp
+++ modules/www/manifests/phpbb.pp
@@ -1,13 +1,20 @@
+# phpBB, the popular and featureful forum software.
+
 class www::phpbb ( $git_root, $root_dir ) {
+  # MySQL database configuration
   $forum_db_name = 'phpbb_sr2013'
   $forum_user = extlookup("phpbb_sql_user")
   $forum_pw = extlookup("phpbb_sql_pw")
 
+  # We require the bindings between php and mysql to work
   package { ['php-mysql']:
     ensure => present,
     notify => Service['httpd'],
   }
 
+  # Checkout of the phpbb installation, with SRs patches against phpbb. One
+  # of these is to delete the installation directory, which is mandatory before
+  # the forum will start serving.
   vcsrepo { "${root_dir}":
     ensure => present,
     user => 'wwwcontent',
@@ -18,6 +25,7 @@ class www::phpbb ( $git_root, $root_dir ) {
     require => Package[ "php", 'php-mysql' ],
   }
 
+  # Create the MySQL db for the forum
   mysql::db { "$forum_db_name":
     user => $forum_user,
     password => $forum_pw,
@@ -25,6 +33,7 @@ class www::phpbb ( $git_root, $root_dir ) {
     grant => ['all'],
   }
 
+  # Load the database data from backup, if it hasn't already.
   exec { 'pop_forum_db':
     command => "mysql -u ${forum_user} --password='${forum_pw}' ${forum_db_name} < /srv/secrets/mysql/phpbb.db; if test $? != 0; then exit 1; fi; touch /usr/local/var/sr/forum_installed",
     provider => 'shell',
@@ -32,6 +41,8 @@ class www::phpbb ( $git_root, $root_dir ) {
     require => Mysql::Db["${forum_db_name}"],
   }
 
+  # Maintain permissions on the config file, and template it. Contains SQL
+  # connection gunge.
   file { "${root_dir}/phpBB/config.php":
     ensure => present,
     owner => 'wwwcontent',
@@ -41,6 +52,7 @@ class www::phpbb ( $git_root, $root_dir ) {
     require => Vcsrepo["${root_dir}"],
   }
 
+  # Directory for storing forum attachments. Not currently backed up, see #1467
   file { "${root_dir}/phpBB/files":
     ensure => directory,
     owner => 'wwwcontent',
@@ -49,6 +61,7 @@ class www::phpbb ( $git_root, $root_dir ) {
     require => Vcsrepo["${root_dir}"],
   }
 
+  # Some form of forum page cache
   file { "${root_dir}/phpBB/cache":
     ensure => directory,
     owner => 'wwwcontent',
@@ -57,6 +70,8 @@ class www::phpbb ( $git_root, $root_dir ) {
     require => Vcsrepo["${root_dir}"],
   }
 
+  # Not the foggiest, but this is how it was on optimus, so this is configured
+  # thus here too.
   file { "${root_dir}/phpBB/store":
     ensure => directory,
     owner => 'wwwcontent',
