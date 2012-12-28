ce143c935b71e6f83f35788ffe7979534402d1c4
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-12-28 19:30:30 +0000

    Comment on how subversion is configured.

--- modules/sr-site/manifests/subversion.pp
+++ modules/sr-site/manifests/subversion.pp
@@ -1,13 +1,20 @@
+# Subversion - which we used quite a large while ago. It's still kept around
+# as it contains a) useful historic data and b) possibly data we still use.
+
 class sr-site::subversion {
-  # Install subversion package(s)
+  # Install subversion package(s), using an imported subversion module
   class {'::subversion': # :: for global namespace
   }
 
+  # Group for owning subversion files. Because puppet is rubbish, the members
+  # never get configured.
   group { 'svn':
     ensure => present,
     members => ['svn', 'apache'],
   }
 
+  # User for owning subversion files. Is /actually/ part of the svn group
+  # because the primary-unix-group (gid) is ocnfigured.
   user { 'svn':
     ensure => present,
     comment => 'Owner of SR SVN repository',
@@ -15,7 +22,7 @@ class sr-site::subversion {
     gid => 'svn',
   }
 
-  # Define the sr svn repo
+  # Define the sr svn repo. Subversion module does all the heavy lifting.
   subversion::svnrepo { 'sr': # /srv/svn is pasted on the front
     owner => 'svn',
     group => 'svn',
@@ -23,6 +30,7 @@ class sr-site::subversion {
     require => User['svn'],
   }
 
+  # If subversion data isn't installed yet, install it.
   exec { 'load-svn':
     command => 'fname=`mktemp /usr/local/var/sr/svn_load_XXXXXX`;\
                 gzip -d -c /srv/secrets/svn/db.gz > $fname;\
@@ -36,6 +44,8 @@ class sr-site::subversion {
     require => [File['/usr/local/var/sr'],Subversion::Svnrepo['sr']],
   }
 
+  # Install historical authorization data. Required to restrict information to
+  # certain files, that are in SVN but shouldn't be public.
   file { '/srv/svn/sr/authfile':
     ensure => present,
     source => 'puppet:///modules/sr-site/authfile',
