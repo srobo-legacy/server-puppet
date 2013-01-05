04f0eb88e4c2178306ed3b6e2e5cd45efe65e01d
Tyler Ward <Tyler+git@scorpia.co.uk>
2013-01-05 11:59:57 -0800

    Added puppet config files for monitoring of services on badger.
    Added a monitoring user to perform checks access is controlled
    by the login/monitoring_ssh_keys file in srv/secrets which only
    allows the monitoring wrapper script to be called.
    
    Checks include all generic service checks and a couple of
    badger specific ones.
    
    Change-Id: Icb1e5c3320332e204d94bdb09221cfebe1974e0f

---
+++ modules/icinga/manifests/init.pp
@@ -0,0 +1,70 @@
+#class icinga {
+
+
+# install generic nagios checks
+  package { 'nagios-plugins-all':
+    ensure => installed,
+  }
+
+  package { 'nagios-plugins-check-updates':
+    ensure => installed,
+  }
+
+# create a user to do the checking
+  user { 'monitoring':
+    ensure => present,
+    comment => 'Monitoring User',
+    shell => '/bin/bash',
+    gid => 'users',
+  }
+
+  file { '/home/monitoring':
+    ensure => directory,
+    owner => 'monitoring',
+    group => 'users',
+    mode => '700',
+    require => User['monitoring']
+  }
+
+  file { '/home/monitoring/.ssh':
+    ensure => directory,
+    owner => 'monitoring',
+    group => 'users',
+    mode => '700',
+  }
+
+  file { '/home/monitoring/.ssh/authorized_keys':
+    ensure => present,
+    owner => 'monitoring',
+    group => 'users',
+    mode => '600',
+    source => '/srv/secrets/login/monitoring_ssh_keys',
+    require => File['/home/monitoring/.ssh'],
+  }
+
+  file { '/srv/monitoring':
+    ensure => directory,
+    owner => 'monitoring',
+    group => 'users',
+    mode => '700',
+    require => User['monitoring']
+  }
+
+  file { '/srv/monitoring/commands.sh':
+    mode => 700,
+    owner => 'monitoring',
+    group => 'users',
+    source => "puppet:///modules/icinga/commands.sh",
+  }
+
+  file { '/srv/monitoring/commands':
+    ensure => directory,
+    recurse => true,
+    source => 'puppet:///modules/icinga/commands',
+    owner => 'monitoring',
+    group => 'users',
+    mode => 700,
+  }
+
+#}
+
