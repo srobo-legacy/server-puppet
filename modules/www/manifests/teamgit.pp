3039a88bdc96bdb3500f97935da4d1233b373ac4
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-01-30 22:29:40 +0000

    Syntax.

--- modules/www/manifests/teamgit.pp
+++ modules/www/manifests/teamgit.pp
@@ -17,7 +17,7 @@ class www::teamgit {
   }
 
   exec { 'create_team_git':
-    command => '/usr/local/bin/team_repos_conf_builder.py /etc/httpd/conf.d/teamgit.conf':
+    command => '/usr/local/bin/team_repos_conf_builder.py /etc/httpd/conf.d/teamgit.conf',
     user => 'root',
   }
 
