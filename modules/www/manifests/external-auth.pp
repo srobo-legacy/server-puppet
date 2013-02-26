e4603c67c4d1566f5da153ea89e5e4c2540e550b
Chris Malton <chrism@cmalton.me.uk>
2013-02-26 18:22:01 +0000

    Remove duplicate Package definition

--- modules/www/manifests/external-auth.pp
+++ modules/www/manifests/external-auth.pp
@@ -1,11 +1,6 @@
 # Primary configuartion for the "External Authentication" system
 
 class www::external-auth ( $git_root, $web_root_dir, $ext_auth_root_dir ) {
-  # external-auth is served through php and some other goo,
-  package { [ "php" ]:
-    ensure => latest,
-    notify => Service[ "httpd" ],
-  }
 
   # Directory permissions and ownership of the external-auth directory. Seeing how
   # /var/www/html belongs to root by default on fedora.
