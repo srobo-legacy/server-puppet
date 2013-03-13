517d5bd7a61f2e3635fb2ee0995750b6c12b03e4
Chris Malton <chrism@cmalton.me.uk>
2013-03-13 15:12:19 +0000

    Configure external authentication server keys.
    See https://www.studentrobotics.org/gerrit/#/c/475/ and
    https://www.studentrobotics.org/trac/ticket/1557 for details of why we need server keys.
    
    Change-Id: Iab3722a39bfc24241a7039f4c8217491201ffddc

--- modules/www/files/ext_auth.config.inc.php
+++ modules/www/files/ext_auth.config.inc.php
@@ -7,6 +7,11 @@ require_once("lib/IDEAuthProvider.php");
 ini_set("session.save_path", dirname(__FILE__) . "/../sessions/");
 ini_set("session.gc_maxlifetime", 3600); //1 hour session time
 
+$priv_key = file_get_contents("/srv/secrets/external-auth/server");
+$pub_key = file_get_contents("/srv/secrets/external-auth/server.pub");
+
+ConfigManager::SetKeys($priv_key, $pub_key);
+
 ConfigManager::SetProvider(new IDEAuthProvider("https://www.studentrobotics.org/ide/"));
 
 ?>
