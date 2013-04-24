328e7d6da66cf22273a86d8f4e21e73f9ebe29fc
Sam Phippen <samphippen@googlemail.com>
2013-04-24 16:33:44 +0100

    Make voting permissions properly octal.
    This was found with puppet-lint.
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>
    
    I73910676d1876f5dfe0bda9d21767d24a2364bb8

--- modules/www/manifests/voting.pp
+++ modules/www/manifests/voting.pp
@@ -14,7 +14,7 @@ class www::voting ($git_root, $web_root_dir) {
     ensure  => directory,
     owner   => 'voting',
     group   => 'users',
-    mode    => '711',
+    mode    => '0711',
     require => User['voting'],
   }
 
@@ -22,7 +22,7 @@ class www::voting ($git_root, $web_root_dir) {
     ensure  => directory,
     owner   => 'voting',
     group   => 'users',
-    mode    => '711',
+    mode    => '0711',
     require => [User['voting'], File['/home/voting']],
   }
 
@@ -51,7 +51,11 @@ class www::voting ($git_root, $web_root_dir) {
     ensure  => directory,
     owner   => 'voting',
     group   => 'users',
-    mode    => '700', # Prohibit people from seeing who voted.
+    mode    => '0700', # Prohibit people from seeing who voted.
+    ensure => directory,
+    owner => 'voting',
+    group => 'users',
+    mode => '0700', # Prohibit people from seeing who voted.
     require => Vcsrepo['/home/voting/public_html/voting'],
   }
 }
