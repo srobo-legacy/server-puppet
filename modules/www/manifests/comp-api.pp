b3151487b9d5bd8d61c75a018beb841a90b91dc9
Peter Law <PeterJCLaw@gmail.com>
2014-03-15 21:33:40 +0000

    Fix #2350, #2362: Add the Competition State API.
    This adds the sr-comp-http Flask based API at /comp-api, including
    a dummy checkout of the state repo.
    
    2014-03-20 Patch set 3: remove unneccesary hyphen from paths, fix
                            incorrect comment in ssl.conf.erb, switch
                            to using canonical repos.
    2014-03-30 Patch set 5: add missing python-simplejson dependency.
    2014-03-30 Patch set 6: change to installing flask from pip so we
                            get a more recent version, specifically
                            one which can cope with jsonifying datetimes.
    
    Change-Id: I48a7b71c976e966a64be178777bec4d1dd181456

---
+++ modules/www/manifests/comp-api.pp
@@ -0,0 +1,65 @@
+# 'comp-api' is the web API to the SRComp library which contains information
+# about the state of the competition
+
+class www::comp-api ( $git_root, $root_dir ) {
+
+  # SimpleJSON so we can handle Decimal()s, PIP so we can get Flask from it.
+  package { ['python-simplejson', 'python-pip']:
+    ensure => present,
+    notify => Service['httpd'],
+    before => Vcsrepo[$root_dir],
+  }
+
+  # need better than version 0.8, which is all that's available in the repos
+  package { ['Flask']:
+    ensure => present,
+    provider => pip,
+    require => Package['python-pip'],
+    notify => Service['httpd'],
+    before => Vcsrepo[$root_dir],
+  }
+
+  # Main checkout of the codebase
+  vcsrepo { $root_dir:
+    ensure    => present,
+    provider  => git,
+    source    => "${git_root}/comp/srcomp-http.git",
+    revision  => 'origin/master',
+    force     => true,
+    owner     => 'wwwcontent',
+    group     => 'apache',
+    require   => Package['Flask', 'python-simplejson'],
+    notify    => Service['httpd'],
+  }
+
+  # Checkout of the competition state
+  $compstate_dir = "${root_dir}/compstate"
+  vcsrepo { $compstate_dir:
+    ensure    => present,
+    provider  => git,
+    source    => "${git_root}/comp/sr2014-comp.git",
+    revision  => 'origin/master',
+    force     => true,
+    owner     => 'wwwcontent',
+    group     => 'apache',
+  }
+
+  # A WSGI config file for serving inside of apache.
+  file { "${root_dir}/comp-api.wsgi":
+    ensure  => present,
+    owner   => 'wwwcontent',
+    group   => 'apache',
+    mode    => '0644',
+    source  => 'puppet:///modules/www/comp-api.wsgi',
+    require => Vcsrepo[$root_dir],
+  }
+
+  # TODO: review this mechanism
+  #  -- should we instead put a post-recieve-pack hook into the repo?
+  cron { 'comp-api-updates':
+    command => "cd ${compstate_dir} && git pull origin master",
+    minute  => '*/5',
+    user    => 'wwwcontent',
+    require => Vcsrepo[$compstate_dir],
+  }
+}
