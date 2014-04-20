cdf26aab1a8355263005db5ac8d78e031b87a0fd
Rob Spanton <rob@robspanton.com>
2014-04-20 22:24:56 +0100

    Configure chronyd
    Set it to serve to any subnet who requests the time -- there's no need
    for any restriction here.  Also set its command password to
    "badgers".  There's little need for that to be 'secure', as it's only
    accessible from localhost.
    
    Change-Id: Ia64125274683804047ecc3afd4e36ab4df8fdf29

--- modules/sr-site/manifests/chronyd.pp
+++ modules/sr-site/manifests/chronyd.pp
@@ -7,7 +7,28 @@ class sr-site::chronyd {
 
   service { "chronyd":
     ensure => running,
-    require => Package[ "chrony" ]
+    require => Package[ "chrony" ],
+    subscribe => [ File[ "chrony.conf" ],
+                   File[ "chrony.keys" ] ],
+  }
+
+  file { "chrony.conf":
+    path => "/etc/chrony.conf",
+    owner => root,
+    group => root,
+    mode => "0644",
+    content => template("sr-site/chrony.conf"),
+    require => [ Package[ "chrony" ],
+                 File[ "chrony.keys" ] ],
+  }
+
+  file { "chrony.keys":
+    path => "/etc/chrony.keys",
+    owner => root,
+    group => chrony,
+    mode => "0640",
+    content => template("sr-site/chrony.keys"),
+    require => Package[ "chrony" ],
   }
 
 }
