7bea9aa59cb3a8b07b0526ee0973e5de1218d9b9
Peter Law <PeterJCLaw@gmail.com>
2014-06-30 21:09:18 +0100

    Fix #2361: Put a userman instance in /root/userman
    This is particularly useful on a development badger, but should also
    be useful on the live one.
    
    2014-07-13 Patch set 2: rebased.
    2014-07-13 Patch set 3: remove unrelated augeas line, swap to single
                            quotes for location string.
    
    Change-Id: Ibd57033d216b840c21e3f540e69f76f77b93351c

---
+++ modules/sr-site/manifests/userman.pp
@@ -0,0 +1,27 @@
+# This adds a checkout of userman.git into the root user's home folder.
+# While not entirely needed on the live server, it's very useful,
+# especially on development badger instances
+
+class sr-site::userman ( $git_root ) {
+  # Checkout of userman's code.
+  $root_dir = '/root/userman'
+  vcsrepo { $root_dir:
+    ensure    => present,
+    provider  => git,
+    source    => "${git_root}/userman.git",
+    revision  => 'origin/master',
+    owner     => 'root',
+    group     => 'root',
+    require   => Package['PyYAML', 'python-ldap'],
+  }
+
+  # local configuration is stored in local.ini
+  $local_ini = "${root_dir}/sr/local.ini"
+  $ldap_manager_pw = extlookup('ldap_manager_pw')
+  file { $local_ini:
+    owner   => 'root',
+    group   => 'root',
+    content => template('sr-site/userman_srusers.ini.erb'),
+    require => Vcsrepo[$root_dir],
+  }
+}
