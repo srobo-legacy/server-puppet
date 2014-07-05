afe4f3b8d1909d00acf7dbee064835ffc06e93f6
Peter Law <PeterJCLaw@gmail.com>
2014-07-05 12:40:44 +0100

    Update the MySQL module.
    This requires a minor change to the config, but is required to make
    things work on Fedora 20.
    
    Change-Id: I1e310ae5e0468c826d37d822c089d874f0d7d885

--- modules/sr-site/manifests/mysql.pp
+++ modules/sr-site/manifests/mysql.pp
@@ -3,7 +3,7 @@ class sr-site::mysql {
 
   # Install the mysql server
   class { "mysql::server":
-    config_hash => { "root_password" => extlookup( "mysql_rootpw" ) }
+    root_password => extlookup('mysql_rootpw'),
   }
 
 }
