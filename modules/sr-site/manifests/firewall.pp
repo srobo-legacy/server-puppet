4f8a3ea702388c9401cd177b3cd94d2f3ed748ce
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-09-11 12:47:51 +0100

    Fix firewall installation for F20
    Fedora now installs firewalld by default, which is incompatible with our
    current configuration. To fix this, disable firewalld and then
    install+enable the iptables service. Require that this occurs before the
    firewall configuration occurs, so that it gets correctly persisted.
    
    Change-Id: Ic98087b6518ae9cb526af32cb64654c7dfa2b5f0

--- modules/sr-site/manifests/firewall.pp
+++ modules/sr-site/manifests/firewall.pp
@@ -2,6 +2,23 @@
 
 class sr-site::firewall {
 
+  # Install standard firewall arrangement, iptables, and disable firewalld
+  # as it isn't useful for us here.
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
+
+  package { 'iptables-services':
+    ensure => latest,
+  }
+
+  service { 'iptables':
+    ensure => running,
+    enable => true,
+    require => [Service['firewalld'], Package['iptables-services']],
+  }
+
   # Purge unmanaged firewall resources
   #
   # This will clear any existing rules, and make sure that only rules
@@ -10,6 +27,11 @@ class sr-site::firewall {
     purge => true
   }
 
-  include sr-site::fw_pre
-  include sr-site::fw_post
+  class { 'sr-site::fw_pre':
+    require => Service['iptables'],
+  }
+
+  class { 'sr-site::fw_post':
+    require => Service['iptables'],
+  }
 }
