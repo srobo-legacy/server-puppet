dbc3c9d32ae2d6d1612c7ed680451e0cc1aa9f9c
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-04 11:42:49 +0100

    Remove superfluous onlyif requirement
    As identified by Alistair
    
    Change-Id: I0de39d09e6bb844353ed9175ad6a47036b602978

--- modules/www/manifests/ide.pp
+++ modules/www/manifests/ide.pp
@@ -132,7 +132,6 @@ class www::ide ( $git_root, $root_dir ) {
     group => 'apache',
     command => "cp -r /srv/secrets/team_status_images/* ${team_status_imgs_live_dir}; touch /usr/local/var/sr/team_status_images_installed",
     creates => '/usr/local/var/sr/team_status_images_installed',
-    onlyif => 'test ! -e /usr/local/var/sr/team_status_images_installed',
     require => File[$team_status_imgs_live_dir],
   }
 
