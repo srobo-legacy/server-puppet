2d8e6529809f63afbc91f1dede7249f0c7f41329
Tom Leese <inbox@tomleese.me.uk>
2015-03-04 19:26:50 +0000

    Improve SRComp deployment
    This means that SRComp uses a virtual environment without sharing site
    pacakges, Pip is used solely to install dependencies and their versions
    are pinned to known working ones. The issue regarding re-installation of
    our own modules has also been fixed.
    
    Our stuff is still installed from their Git repo and they also clone
    from 'origin/master' which is fine for the moment since they're all
    in maintainer mode. However, in the future, these repos should be put
    onto PyPI and we can use exact versions. That change will also reduce
    the amount of stuff in Puppet since everything can be specified in the
    'requirements.txt' file.
    
    Patch Set #3: Improve wording in commit message.
    
    Change-Id: I9b3ba97b3e5185116884f7949cc035f295630eed

--- modules/www/manifests/comp-api.pp
+++ modules/www/manifests/comp-api.pp
@@ -3,72 +3,12 @@
 
 class www::comp-api ( $git_root, $root_dir ) {
 
-  # SimpleJSON so we can handle Decimal()s,
-  package { ['python-simplejson',
-             # PIP so we can get Flask from it.
-             'python-pip',
-             # Virtualenv so we can install the srcomp stuff into one.
-             'python-virtualenv',
-             # We use dateutil for timezone munging
-             'python-dateutil' ]:
-    ensure => present,
-    notify => Service['httpd'],
-    before => Exec['srcomp-venv'],
-  }
-
-  # Need more recent than version 0.8, which is all that's available in the F17 repos
-  # Ensure the Fedora-provided package is removed
-  package { 'python-flask':
-    ensure => absent,
-  }
-
-  # Get it from pip instead
-  package { ['Flask']:
-    ensure => present,
-    provider => pip,
-    require => [ Package['python-pip'],
-                 Package['python-flask'] ],
-    notify => Service['httpd'],
-  }
-
   # Containing folder
   file { $root_dir:
     ensure  => directory,
     force   => true,
     owner   => 'wwwcontent',
     group   => 'apache',
-  }
-
-  # Virtual environment
-  $venv_dir = "${root_dir}/venv"
-  exec { 'srcomp-venv':
-    command => "virtualenv --system-site-packages --python=python2.7 '${venv_dir}'",
-    cwd     => $root_dir,
-    creates => $venv_dir,
-    path    => ['/usr/bin'],
-    user    => 'wwwcontent',
-    group   => 'apache',
-    require => [Package['python-virtualenv'],File[$root_dir]],
-  }
-
-  srcomp_repo { 'ranker':
-    git_root  => $git_root,
-    root_dir  => $root_dir,
-    venv_dir  => $venv_dir,
-  }
-
-  srcomp_repo { 'srcomp':
-    git_root  => $git_root,
-    root_dir  => $root_dir,
-    venv_dir  => $venv_dir,
-    require   => Srcomp_repo['ranker'],
-  }
-
-  srcomp_repo { 'srcomp-http':
-    git_root  => $git_root,
-    root_dir  => $root_dir,
-    venv_dir  => $venv_dir,
-    require   => [Package['Flask'],Srcomp_repo['srcomp']],
   }
 
   # Syslog configuration, using local1
@@ -117,6 +57,6 @@ class www::comp-api ( $git_root, $root_dir ) {
     group   => 'apache',
     mode    => '0644',
     content => template('www/comp-api.wsgi.erb'),
-    require => [File[$root_dir],Srcomp_repo['srcomp-http']],
+    require => [File[$root_dir]],
   }
 }
