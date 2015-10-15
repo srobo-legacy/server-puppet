91c57b17f08522bc3059e6ce711665f1b4960d98
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-10-15 22:46:00 +0000

    Install RT local site config
    Load some relevant variables, and don't pass the db password on the
    command line any more: it'll be loaded from the config file

--- modules/sr_site/manifests/requesttracker.pp
+++ modules/sr_site/manifests/requesttracker.pp
@@ -4,6 +4,10 @@
 class sr_site::requesttracker ( ) {
   require sr_site::mysql
 
+  # Load some web related variables
+  $www_base_hostname = hiera('www_base_hostname')
+  $www_canonical_hostname = hiera('www_canonical_hostname')
+
   # Install relevant packages
   package {['rt', 'rt-mailgate']:
     ensure => present,
@@ -21,6 +25,15 @@ class sr_site::requesttracker ( ) {
     grant => ['all'],
   }
 
+  # Specify general config: connection params etc
+  file { '/etc/rt/RT_SiteConfig.pm':
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '664',
+    content => template('sr_site/RT_SiteConfig.pm.erb'),
+  }
+
   # Populate the database. On a development machine, what you want is to have
   # a freshly initialized DB that can be customised as required -- while on
   # the deployment machine, we only ever want the deployment database.
@@ -28,12 +41,13 @@ class sr_site::requesttracker ( ) {
     # Insert all relevant data into the database, without creating it.
     # Not passing the pw on the command line is a problem for another time.
     exec { 'initialize-rt':
-      command => "/usr/sbin/rt-setup-database --action init --dba $rt_db_user --dba-password $mysql_db_pw --skip-create && touch /usr/local/var/sr/rt_installed",
+      command => "/usr/sbin/rt-setup-database --action init --skip-create && touch /usr/local/var/sr/rt_installed",
       provider => 'shell',
       creates => '/usr/local/var/sr/rt_installed',
-      require => Mysql::Db[$rt_db_name],
+      require => [Mysql::Db[$rt_db_name], File['/etc/rt/RT_SiteConfig.pm']],
     }
   } else {
     # XXX unimplemented, restore from backup.
   }
+
 }
