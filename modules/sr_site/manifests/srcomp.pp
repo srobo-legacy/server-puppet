17eea78ad5b1c36f158f5e472d2f16f5c49b10c5
Peter Law <PeterJCLaw@gmail.com>
2016-03-31 12:17:35 +0100

    Switch to SR2016 compstate
    Unfortunately puppet doesn't actually seem to replace the repos
    which this affects, though I suspect that this is a result of us
    using a rather old version of the Vcsrepo module. Whatever the
    reason we'll need to manually swap the repos, which I'll do once
    this is merged.
    
    Change-Id: Ib752b29d7c670867fb0e6a7bfb4a4e45579c7b2c

--- modules/sr_site/manifests/srcomp.pp
+++ modules/sr_site/manifests/srcomp.pp
@@ -108,7 +108,7 @@ class sr_site::srcomp($git_root,
   vcsrepo { $ref_compstate:
     ensure    => bare,
     provider  => git,
-    source    => "${git_root}/comp/sr2015-comp.git",
+    source    => "${git_root}/comp/sr2016-comp.git",
     user      => 'srcomp',
     require   => [User['srcomp'],File[$srcomp_home_dir]],
   }
